df91e94d003d22cb71bf84881780945375892fdf
GoodNight <goodnightlilanhui@gmail.com>
2021-06-22 15:18:34 +0800

    feat: add docker

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,50 @@
 <template>
-  <div>
-    hello
+  <div class="docker">
+    <div class="docker__item docker__item--active">
+      <div class="iconfont">&#xe9db;</div>
+      <div class="docker__title">首页</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe631;</div>
+      <div class="docker__title">购物车</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe61e;</div>
+      <div class="docker__title">订单</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe660;</div>
+      <div class="docker__title">我的</div>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
+.docker {
+  display: flex;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 0.49rem;
+  border-top: 1px solid #f1f1f1;
+  &__item {
+    flex: 1;
+    text-align: center;
+    color: #333333;
+    .iconfont {
+      margin: 0.07rem 0 0.02rem 0;
+      font-size: 0.18rem;
+    }
+    &--active {
+      color: #1FA4FC;
+    }
+  }
+  &__title {
+    font-size: 20px;
+    transform: scale(0.5, 0.5);
+    transform-origin: center top;
+  }
+}
+
 </style>
