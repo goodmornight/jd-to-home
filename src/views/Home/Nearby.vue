a4ea6ae267c0dcd5b488814e57de6c6207c11e59
GoodNight <goodnightlilanhui@gmail.com>
2021-06-22 19:40:41 +0800

    refactor: Home

---
+++ src/views/Home/Nearby.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="nearby">
+    <div class="nearby__title">附近店铺</div>
+    <div class="nearby__item">
+      <img
+        src="http://www.dell-lee.com/imgs/vue3/near.png"
+        class="nearby__item__img"
+      />
+      <div class="nearby__content">
+        <div class="nearby__content__title">沃尔玛</div>
+        <div class="nearby__content__tags">
+          <span class="nearby__content__tag">月售1万+</span>
+          <span class="nearby__content__tag">起送¥0</span>
+          <span class="nearby__content__tag">起送¥0</span>
+        </div>
+        <p class="nearby__content__highlight">
+          VIP尊享满89元减4元运费券（每月3张）
+        </p>
+      </div>
+    </div>
+    <div class="nearby__item">
+      <img
+        src="http://www.dell-lee.com/imgs/vue3/near.png"
+        class="nearby__item__img"
+      />
+      <div class="nearby__content">
+        <div class="nearby__content__title">沃尔玛</div>
+        <div class="nearby__content__tags">
+          <span class="nearby__content__tag">月售1万+</span>
+          <span class="nearby__content__tag">起送¥0</span>
+          <span class="nearby__content__tag">起送¥0</span>
+        </div>
+        <p class="nearby__content__highlight">
+          VIP尊享满89元减4元运费券（每月3张）
+        </p>
+      </div>
+    </div>
+    <div class="nearby__item">
+      <img
+        src="http://www.dell-lee.com/imgs/vue3/near.png"
+        class="nearby__item__img"
+      />
+      <div class="nearby__content">
+        <div class="nearby__content__title">沃尔玛</div>
+        <div class="nearby__content__tags">
+          <span class="nearby__content__tag">月售1万+</span>
+          <span class="nearby__content__tag">起送¥0</span>
+          <span class="nearby__content__tag">起送¥0</span>
+        </div>
+        <p class="nearby__content__highlight">
+          VIP尊享满89元减4元运费券（每月3张）
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Nearby'
+}
+</script>
+
+<style lang="scss">
+@import "../../style/viriables.scss";
+.nearby {
+  &__title {
+    line-height: 0.25rem;
+    font-size: 0.18rem;
+    color: $content-fontcolor;
+    font-weight: normal;
+    margin: 0.16rem 0 0.02rem 0;
+  }
+  &__item {
+    display: flex;
+    padding-top: 0.12rem;
+    &__img {
+      width: 0.56rem;
+      height: 0.56rem;
+      margin-right: 0.16rem;
+    }
+  }
+  &__content {
+    flex: 1;
+    padding-bottom: 0.12rem;
+    border-bottom: 1px solid $content-bgColor;
+    &__title {
+      line-height: 0.22rem;
+      font-size: 0.16rem;
+      color: $content-fontcolor;
+    }
+    &__tags {
+      line-height: 0.18rem;
+      font-size: 0.13rem;
+      color: $content-fontcolor;
+      margin-top: 0.08rem;
+    }
+    &__tag {
+      margin-right: 0.16rem;
+    }
+    &__highlight {
+      margin: 0.08rem 0 0 0;
+      line-height: 0.18rem;
+      font-size: 0.13rem;
+      color: #e93b3b;
+    }
+  }
+}
+</style>
